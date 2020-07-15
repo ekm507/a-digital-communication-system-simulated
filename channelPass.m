8faff5a1a76d524a45d0c5f6811014702cc6ac55
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2020-07-15 23:49:34 +0430

    docs

--- channelPass.m
+++ channelPass.m
@@ -1,10 +1,26 @@
+% pass signal through channel.
+
+% for now, channel will add noise and shift signal.
+% output signal = f(signal to pass through channel, Signal to Noise Ratio, size of shift in signal in samples)
+
 function outputSignal = channelPass(inputSignal, SNR, shiftSize)
+    % add noise to the signal.
     noisySignal = awgn(inputSignal, SNR);
+
+    % if you are going to miss initial samples of signal
     if shiftSize > 0
+        % shift signal to the left
         shiftedSignal = noisySignal(shiftSize + 1 : end);
+    
+    % or if you are receiving the signal with a delay
     else
+        % generate empty signal with added noise
         initialSignal = awgn(zeros([1 -shiftSize]), 10);
+
+        % shift the signal to the right
         shiftedSignal = [initialSignal noisySignal];
     end
+
+    % output will be the shifted signal
     outputSignal = shiftedSignal;
 end
--- diffCode.m
+++ diffCode.m
@@ -1,12 +1,29 @@
+% this function does a differential coding to data
+% output data = f(data you want to encode, M (How big a number in data can be. output will be modulo to M))
 function diffCoded_data = diffCode(data, M)
+
+    % get size of data. 
     q = size(data);
+
+    % preallocate output data.
     temp_out = zeros(q);
+
+    % so q(2) will be size of data
     q = q(2);
 
+    % latest number added to output data
     last_num = 0;
+
+    % itterate over 1 to size of input data
     for i = 1:q
+        % generate differential coded number.
+        % it will be sum of latest added number and new number. modulo by M
         last_num = mod(last_num + data(i), M);
+
+        % add diff coded number to output data
         temp_out(i) = last_num;
     end
+
+    % return output data
     diffCoded_data = temp_out;
 end
