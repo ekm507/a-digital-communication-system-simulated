eccd8d8a42eae7daf9bf9257229f2210de905150
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2020-07-22 23:17:38 +0430

    docs

--- parityAdd.m
+++ parityAdd.m
@@ -1,13 +1,37 @@
+% add parity numbers to data.
+% but this is in a general form.
+% so this is not actual parity always. ( this is parity when M is 2)
+% but this is sum of a block, modulo to M
+% this works like a general form of xor operator in base 2. but it works in other bases as well.
+
+% data with parity numbers added to it = f( data to add parity to it,
+% size of a block to add parity, M (PSK modulation size - also data numbers base.) )
 function parityAddedData = parityAdd(data, ParityBlockSize, M)
 
+    % get size of data
     q = size(data);
     q = q(2);
+
+    % parity added data will be stored here
     temp_output = [];
+
+    % itterate over bits in data. jumping in size of a block to check parity of
     for i = 1 : ParityBlockSize : q - ParityBlockSize  +1
+
+        % get a data block
         b = data(i : i + ParityBlockSize - 1);
+
+        % calculate the parity. it is modulo of sum of all the numbers in a block to M
         c = mod(sum(b), M);
+
+        % add parity number to end of the block.
+        % and add new blocks to output data 
         temp_output = [temp_output b c];
+    
+    % adding parity numbers to data done.
     end
 
+    % return data with added parity numbers to each block of it
     parityAddedData = temp_output;
+    
 end
--- parityCheck.m
+++ parityCheck.m
@@ -1,20 +1,54 @@
+% remove parity numbers from data.
+% and check for any errors
+
+% this is a parity checking
+% but this is in a general form.
+% so this is not actual parity always. ( this is parity when M is 2)
+% but this is sum of a block, modulo to M
+% this works like a general form of xor operator in base 2. but it works in other bases as well.
+
+% data with parity numbers removed from it= f( data to remove parity from it and check for errors,
+% size of a parity block( this is the same number you provide to parityAdd function),
+% M (PSK modulation size - also data numbers base.) )
 function outputData = parityCheck (inputData, ParityBlockSize, M)
 
+    % data with parity numbers removed from, will be stored here.
     ParityCheckedData = [];
+
+    % get size of input data
     q = size(inputData);
     q = q(2);
 
+    %
     a = mod(q + 1 - 1, ParityBlockSize + 1);
     max_decodable = q - ParityBlockSize  - a;
 
+    % itterate over numbers in each block of data
     for i = 1 : ParityBlockSize + 1 : max_decodable
+
+        % get a block of data
         b = inputData(i : i + ParityBlockSize - 1);
+
+        % calculate parity of it
         c = mod(sum(b), M);
+
+        % so if there are conflicts with parity number provided by parityAdd functions
         if c ~= inputData(i + ParityBlockSize)
+            % do whatever you want to do!
+            % errors can't be corrected yet!
+            % as an example you can count number of errors and calculate quality of system
         %    disp(i);
+
+        % checking for conflicts done
         end
+
+        % remove that parity number from data and add cleaned block to output
         ParityCheckedData = [ParityCheckedData b];
+    
+    % checlong for parity numbers and removing them from data done.
     end
 
+    % return data with parity numbers removed.
     outputData = ParityCheckedData;
+    
 end
