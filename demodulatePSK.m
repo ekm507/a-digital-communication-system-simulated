1ec7d5b69e22fb9bf455c9a1b7e3c9d9a4ea43fe
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2020-07-23 22:55:34 +0430

    increase performance

--- demodulatePSK.m
+++ demodulatePSK.m
@@ -3,14 +3,15 @@
 
 function outputPhasors = demodulatePSK(inputSignal, M, signalLength, sampling_frequency, carrier_frequency)
 
-    q = size(inputSignal);
-    q = q(2);
+    % get size of input signal
+    q0 = size(inputSignal);
+    q0 = q0(2);
 
     % generate sin and cos signals (carrier signals) with size of input signal.
     % TODO: simulate some imperfections
     
     % generate time domain
-    T = 0:1/sampling_frequency:q/sampling_frequency - 1/sampling_frequency;
+    T = 0:1/sampling_frequency:q0/sampling_frequency - 1/sampling_frequency;
 
     % carrier with phase = 0
     carrierS = sin(carrier_frequency * 2*pi *T);
@@ -27,34 +28,27 @@ function outputPhasors = demodulatePSK(inputSignal, M, signalLength, sampling_fr
     sigC = inputSignal .* carrierC;
 
     % output phasors will be stored here
-    output_dem = zeros(1, floor(length(inputSignal) / q) +1);
+    output_dem = zeros(1, floor(length(inputSignal) / q0) +1);
 
     % get size of each signal block. (size of each pulse in samples)
     q = signalLength * sampling_frequency + 1;
 
+    % get number of pulses in signal.
+    n = q0 / q;
+
+    % seperate sine multiplied signal into pulses
+    Blocks_of_sigS = reshape(sigS, q, n);
+
+    % seperate cosine multiplied signal into pulses
+    Blocks_of_sigC = reshape(sigC, q, n);
+
+    % get mean of each signal block. mean works like integral.
+    % having these 2 means, we can calculate PSK phasors for each pulse
+    pS = mean(Blocks_of_sigS);
+    pC = mean(Blocks_of_sigC);
     
-    % Demodulation process starts
-    for k = 1:q:length(inputSignal)
-        if k+q-1 > length(inputSignal)
-            break;
-        end
-
-        % get integral of each block of multiplied signals.
-
-        % mean of each block multiplied by carrier with phase = 0
-        Multiplied_SignalS = mean(sigS(k:k+q-1));
-
-        % mean of each block multiplied by carrier with phase = π
-        Multiplied_SignalC = mean(sigC(k:k+q-1));
-        
-        % convert two numbers to a phasor
-        phasor = (Multiplied_SignalS + Multiplied_SignalC * (0+1i));
-
-        % add phasor to the list
-        output_dem(floor(k/q)+1) = phasor;
-    end
-    %scatterplot(output_dem)
-
+    
     % output all phasors
-    outputPhasors = output_dem;
+    outputPhasors = pS + pC * (0 + 1i);
+
 end
