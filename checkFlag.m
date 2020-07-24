fb7f230f9a404b69133f2ff8078cd0bd08b7c244
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2020-07-24 05:25:31 +0430

    docs

--- checkFlag.m
+++ checkFlag.m
@@ -71,10 +71,15 @@ function outputData = checkFlag (inputData, flagSize)
     q = size(inputData);
     q = q(2);
 
-    lastq = 1;
+    % latest size of data will be stored here. (at each level of cutting data.)
+    lastq = q;
+
     % while there is still data unchecked
     while q > 1
+
+        % number of cascaded ones seen.
         numberOfOnes = 0;
+
         % itterate over bits in input data
         for b = 1:q
 
@@ -102,6 +107,7 @@ function outputData = checkFlag (inputData, flagSize)
 
                 % flag is found. no need to continue itterating.
                 break
+        
             % checking for if there is a flag is done.
             end
 
@@ -111,19 +117,31 @@ function outputData = checkFlag (inputData, flagSize)
         % get new size of data. because initial part of it is cleaned now.
         q = size(inputData);
         q = q(2);
+
+        % if no flag is found anymore, it means that we are at the end of the data.
+        % if you are at the final block of the data
         if lastq == q
+
+            % add anything left to array.
             FlagCheckedData = [FlagCheckedData, inputData];
+
+            % and break the whole process. because there is no data left.
             break
+
+        % adding final block to data is done.
         end
+
+        % save latest size of data. so this way we can know if we have reached the final block.
         lastq = q;
+    
+
     % separating data in blocks done.
     end
 
 
     % start cleaning data
 
-    q = size(FlagCheckedData);
-    q = q(1);
+    % output data will be stored here. seperated by blocks.
     outputData = {};
 
 
@@ -131,50 +149,85 @@ function outputData = checkFlag (inputData, flagSize)
     for k = FlagCheckedData
     % for each block do:
 
+        % first convert each cell to a matrix so we can process it.
         k = cell2mat(k);
         
+        % for each block, decoded data will be stored here.
         tmpOutData = [];
+
+        % we use a buffer to add decoded numbers to list.
+        % thats for checking for similarities to flag.
+        % so at first of each block buffer is empty.
         buffer = [];
         
         % get size of a block
         q = size(k);
         q = q(2);
 
+        % number of cascaded ones seen.
+        % at the begining of each block, it is zero.
         numberOfOnes = 0;
 
         % itterate over numbers in a block
         for b = 1:q
 
-
             % so if bit was a 1
             if k(b) == 1
+            
                 % add ones counter by 1
                 numberOfOnes = numberOfOnes + 1;
 
+                % and add the number (which is ,of course, 1) to the buffer.
                 buffer = [buffer k(b)];
+            
+            % but if the number was not a 1
             else
 
-                if numberOfOnes ==flagSize - 1
+                % if there are enough ones cascaded, so we have found a similarity
+                if numberOfOnes == flagSize - 1
+
+                    % add buffer to output list. but ignoring the number right after the buffer.
                     tmpOutData = [tmpOutData buffer];
-                    buffer = [];                        
+
+                    % buffer is added. so empty the buffer.
+                    buffer = [];
+
+                % but if everything is normal and there are not too much ones cascaded               
                 else
 
+                    % add buffer to output list. add this number seed too.
                     tmpOutData = [tmpOutData buffer k(b)];
+
+                    % buffer is added to the output list. so empty it.
                     buffer = [];
+                
+                % avoiding flag similarity adds, done.
                 end
 
+                % the number we have seen is not 1. so reset the counter.
                 numberOfOnes = 0;
+
+            % adding number to output list done.
             end
 
 
         % cleaning this data block done
         end
+
+        % at the end of decoding a block, buffer might be not empty.
+        % this happens when latest number in a block was a 1
+        % so lets just add anything left to output list.
         tmpOutData = [tmpOutData buffer];
-        tmpOutData;
+
+        % so after that, each cleaned block should be added to whole data.
+        % we add this to a cell array. to be able to detect and avoid errors in decoding.
+        % add this block to output data
         outputData = [outputData tmpOutData];
     
     % cleaning all data blocks done
     end
+
+    % this is going to be returned
     outputData;
 
 % checking flags and cleaning data done.
